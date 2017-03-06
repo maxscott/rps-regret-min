08dd4bc73887195a7b286ceec4a4f048b906d5af
Max <max.weinbrown@gmail.com>
2017-03-05 23:56:46 -0500

    compute regrets for actions

--- lib/rps.ex
+++ lib/rps.ex
@@ -8,32 +8,56 @@ defmodule RPS do
   @paper 1
   @scissors 2
 
+  @actions_count 3
+
   def rock, do: @rock
   def paper, do: @paper
   def scissors, do: @scissors
 
+  def regrets_for_action(our_action, opponent_action) do
+    temp = List.duplicate(0, @actions_count)
+
+    cond do
+      # all ties
+      our_action == opponent_action ->
+        temp = List.replace_at(temp, our_action-1, -1)
+        List.replace_at(temp, our_action-@actions_count+1, +1)
+
+      # all wins
+      rem(our_action - opponent_action + 3, 3) == 1 ->
+        temp = List.replace_at(temp, our_action-1, -1)
+        List.replace_at(temp, our_action-@actions_count+1, -2)
+
+      # all losses
+      rem(opponent_action - our_action + 3, 3) == 1 ->
+        temp = List.replace_at(temp, our_action-1, 2)
+        List.replace_at(temp, our_action-@actions_count+1, 1)
+    end
+
+  end
+
   # aka, "normalize list based on sum"
-  def strategyForRegrets(regrets) when is_list(regrets) do
+  def strategy_for_regrets(regrets) when is_list(regrets) do
     sum = Enum.sum(regrets)
     Enum.map(regrets, fn(x) -> x/sum end)
   end
 
-  def actionForStrategy(strategy) when is_list(strategy) do
+  def action_for_strategy(strategy) when is_list(strategy) do
     strategy
       |> accumulate
-      |> chooseActionUniform
+      |> choose_action_uniform
   end
 
-  def chooseActionUniform(distribution) when is_list(distribution) do
+  def choose_action_uniform(distribution) when is_list(distribution) do
     seed = :rand.uniform
-    chooseActionWithIndex(distribution, seed)
+    choose_action_with_index(distribution, seed)
   end
 
-  def chooseActionWithIndex([ head | tail ], seed, count \\ 0) do
+  def choose_action_with_index([ head | tail ], seed, count \\ 0) do
     if head >= seed do
       {count, seed}
     else
-      chooseActionWithIndex(tail, seed, count+1)
+      choose_action_with_index(tail, seed, count+1)
     end
   end
 
--- test/rps_test.exs
+++ test/rps_test.exs
@@ -3,7 +3,7 @@ defmodule RPSTest do
   doctest RPS
 
   test "given a strategy, yield an action" do
-    {index, seed} = RPS.actionForStrategy([0.4, 0.3, 0.3])
+    {index, seed} = RPS.action_for_strategy([0.4, 0.3, 0.3])
     correct = cond do
       seed <= 0.4 ->
         index == 0
@@ -18,19 +18,28 @@ defmodule RPSTest do
   test "given distribution, choose action for seed value" do
     dist = [0.4, 0.7, 1.0]
 
-    assert RPS.chooseActionWithIndex(dist, 0.4) == {0, 0.4}
-    assert RPS.chooseActionWithIndex(dist, 0.7) == {1, 0.7}
-    assert RPS.chooseActionWithIndex(dist, 0.8) == {2, 0.8}
+    assert RPS.choose_action_with_index(dist, 0.4) == {0, 0.4}
+    assert RPS.choose_action_with_index(dist, 0.7) == {1, 0.7}
+    assert RPS.choose_action_with_index(dist, 0.8) == {2, 0.8}
   end
 
   test "given accumulated regrets, yield strategy" do
-    assert RPS.strategyForRegrets([3, 1, 4]) == [3/8, 1/8, 4/8]
+    assert RPS.strategy_for_regrets([3, 1, 4]) == [3/8, 1/8, 4/8]
   end
 
-  @tag :skip
   test "compute regret of first action in light of second action" do
     # [rock, paper, scissors]
     # [0, 1, 2]
-    assert RPS.regretForAction(RPS.rock, RPS.paper) == [0, 1, 2]
+    assert RPS.regrets_for_action(RPS.rock, RPS.paper) ==       [0,  1,  2]
+    assert RPS.regrets_for_action(RPS.rock, RPS.rock) ==        [0,  1, -1]
+    assert RPS.regrets_for_action(RPS.rock, RPS.scissors) ==    [0, -2, -1]
+
+    assert RPS.regrets_for_action(RPS.paper, RPS.rock) ==       [-1, 0, -2]
+    assert RPS.regrets_for_action(RPS.paper, RPS.paper) ==      [-1, 0,  1]
+    assert RPS.regrets_for_action(RPS.paper, RPS.scissors) ==   [ 2, 0,  1]
+
+    assert RPS.regrets_for_action(RPS.scissors, RPS.rock) ==     [ 1,  2, 0]
+    assert RPS.regrets_for_action(RPS.scissors, RPS.scissors) == [ 1, -1, 0]
+    assert RPS.regrets_for_action(RPS.scissors, RPS.paper) ==    [-2, -1, 0]
   end
 end
